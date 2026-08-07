e1e9023bf372aa33eacc72df11dbd14d11dfea96
Djore_017 <138606159+Jore-X@users.noreply.github.com>
2026-08-07 02:52:36 -0300

    Update [code].js

--- functions/[code].js
+++ functions/[code].js
@@ -1,18 +1,25 @@
-import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = "https://jozbcmeraswixnosofua.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvemJjbWVyYXN3aXhub3NvZnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYwNDYwOTcsImV4cCI6MjEwMTYyMjA5N30.D9Rapt3izdG7tiHIY0CMPUEeF1z8w8zF1EPFy9Bm3Bw";
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export async function onRequest(context) {
   const code = context.params.code;
-  const { data } = await supabaseClient
-    .from("links")
-    .select("*")
-    .eq("code", code)
-    .maybeSingle();
+
+  const response = await fetch(`${supabaseUrl}/rest/v1/links?code=eq.${code}`, {
+    headers: {
+      apikey: supabaseKey,
+      Authorizzation: `Bearer ${supabaseKey}`,
+    },
+  });
+
+  const data = await response.json();
 
   console.log(code);
-  return Response.redirect(data.original_url, 302);
+  console.log(data);
+  return new Response(JSON.stringify(data));
+
+  const link = data[0];
+  console.log(link);
+
+  //   return Response.redirect(link.original_url, 302);
 }
